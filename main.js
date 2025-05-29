7a0358a1937d02b3edebfc40bcf98d360495a6c8
Dilli babu kumaravel <dillibabu@vertace.com>
2025-05-29 15:54:18 +0530

    elctron app setup

---
+++ main.js
@@ -0,0 +1,42 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  // Load your React app
+  // In development, this will be your development server
+  // In production, this will be the built index.html file
+  const startUrl = process.env.ELECTRON_START_URL || url.format({
+    pathname: path.join(__dirname, '/build/index.html'),
+    protocol: 'file:',
+    slashes: true
+  });
+
+  win.loadURL(startUrl);
+
+  // Open the DevTools.
+  // win.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+}); 
