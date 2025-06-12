8eb6a7f85d1803de46db43b8f2f99443c2fa5499
Dilli babu kumaravel <dillibabu@vertace.com>
2025-06-12 12:53:00 +0530

    api call changes

--- src/services/api/useApiActions.ts
+++ src/services/api/useApiActions.ts
@@ -9,21 +9,19 @@ import {
   getApiRouteUnit,
 } from "../../helpers/Common_functions";
 
-// Define action types
-type ProductAction = "GetAll" | "Create" | "Update" | "Delete";
-type VariantAction = "Get" | "Create" | "Update";
-type CategoryAction = "Get" | "Create" | "Update";
-type UnitAction = "Get" | "Create";
 type Action = "GetAll" | "Create" | "Update" | "Delete";
 
-// Generic fetcher with useCallback + identifier support
 type FetcherWithIdentifier<T extends string> = {
   (action: T, data?: any): Promise<void>;
   getIdentifier: (action: T) => string;
 };
 
 function createFetcher<T extends string>(
-  getRouteFn: (action: T) => { method: string; endpoint: string; identifier: string }
+  getRouteFn: (action: T) => {
+    method: string;
+    endpoint: string;
+    identifier: string;
+  }
 ): (dispatch: any) => FetcherWithIdentifier<T> {
   return (dispatch: any) => {
     const identifierMap: Record<T, string> = {} as Record<T, string>;
@@ -59,10 +57,10 @@ function createFetcher<T extends string>(
 export const useApiActions = () => {
   const dispatch = useDispatch();
 
-  const ProductsApi = createFetcher<ProductAction>(getApiRouteProduct)(dispatch);
-  const VariantsApi = createFetcher<VariantAction>(getApiRouteVariant)(dispatch);
-  const CategoriesApi = createFetcher<CategoryAction>(getApiRouteCategory)(dispatch);
-  const UnitsApi = createFetcher<UnitAction>(getApiRouteUnit)(dispatch);
+  const ProductsApi = createFetcher<Action>(getApiRouteProduct)(dispatch);
+  const VariantsApi = createFetcher<Action>(getApiRouteVariant)(dispatch);
+  const CategoriesApi = createFetcher<Action>(getApiRouteCategory)(dispatch);
+  const UnitsApi = createFetcher<Action>(getApiRouteUnit)(dispatch);
 
   return {
     ProductsApi,
