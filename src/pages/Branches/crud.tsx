7bd8c2bf5ea6c163b0739ceccc3733e4b920a8f2
dilli babu <dilli4904@gmail.com>
2025-06-25 06:57:12 +0530

    new chnages

--- src/pages/Branches/crud.tsx
+++ src/pages/Branches/crud.tsx
@@ -1,27 +1,17 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { GenericCrudPage } from '../../components/common/GenericCrudPage';
 import { getEntityApiRoutes } from '../../helpers/CrudFactory';
 import { branchesFormItems } from './formItems';
 import { brancheColumns } from './columns';
 import { useApiActions } from '../../services/api/useApiActions';
 import { useDynamicSelector } from '../../services/redux/selector';
-import { useGenericCrud } from '../../hooks/useGenericCrud';
 
-const BrachesCrud: React.FC = () => {
+const BranchesCrud: React.FC = () => {
   const { getEntityApi } = useApiActions();
   const OrganisationsApi = getEntityApi('Organisations');
-  const { items: organisationItems, loading: orgLoading } = useDynamicSelector(
+  const { items: organisationItems } = useDynamicSelector(
     OrganisationsApi.getIdentifier('GetAll')
   );
-
-  // Use the hook and get setFilterValues
-  const crud = useGenericCrud({
-    title: 'Branches',
-    columns: brancheColumns,
-    formItems: branchesFormItems(organisationItems?.result),
-    apiRoutes: getEntityApiRoutes('Braches'),
-    formColumns: 2,
-  });
 
   return (
     <GenericCrudPage
@@ -32,9 +22,8 @@ const BrachesCrud: React.FC = () => {
         apiRoutes: getEntityApiRoutes('Braches'),
         formColumns: 2,
       }}
-      onFilterChange={crud.setFilterValues}
     />
   );
 };
 
-export default BrachesCrud;
+export default BranchesCrud;
