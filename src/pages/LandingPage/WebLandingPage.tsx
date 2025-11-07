daed85c8e3cc8e54db637aa4073465f6f8a242b1
dilli babu <dilli4904@gmail.com>
2025-11-07 11:08:11 +0530

    invoice

--- src/pages/LandingPage/WebLandingPage.tsx
+++ src/pages/LandingPage/WebLandingPage.tsx
@@ -48,7 +48,7 @@ const WebLandingPage: React.FC = () => {
     });
 
     // Web browser download
-    const downloadUrl = 'https://freshfocuzstudio.s3.ap-south-1.amazonaws.com/SubrivaBilling/SubrivaBilling-V.1.0.1.exe';
+    const downloadUrl = 'https://freshfocuzstudio.s3.ap-south-1.amazonaws.com/SubrivaBilling/SubrivaBilling+Setup+2.0.2.exe';
     const link = document.createElement('a');
     link.href = downloadUrl;
     link.download = 'SubrivaBilling Setup 1.0.1.exe';
